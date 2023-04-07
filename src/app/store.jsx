70da08829040b1e0e9c20afd27c089b79b3c2b43
yıldız <mesel4461@gmail.com>
2023-04-08 02:33:29 +0300

    as

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 import { createStore } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
@@ -12,7 +22,14 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   // devTools: process.env.NODE_ENV !== "production",
 });
 

530da4ccff4781a9b7877a6c4d135f337b5c68dd
yıldız <mesel4461@gmail.com>
2023-04-08 03:11:06 +0300

    as

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,7 +1,15 @@
+import { Button, Typography } from "@mui/material";
 import React from "react";
 
 const Firms = () => {
-  return <div>Firm</div>;
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+    </div>
+  );
 };
 
 export default Firms;

7c652dbff4b11cbcd0ac44800502180276b942a9
yıldız <mesel4461@gmail.com>
2023-04-15 02:04:53 +0300

    as

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,3 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = createSlice({});
+const authSlice = createSlice({
+  name: "auth",
+
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+  },
+});

0f0a03a0b35bd43f6b317beba48497fed1bb1b91
yıldız <mesel4461@gmail.com>
2023-04-15 02:15:59 +0300

    as

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -7,5 +7,17 @@ const authSlice = createSlice({
     currentUser: null,
     loading: false,
     error: false,
+    isAdmin: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      (state.loading = true), (state.error = false);
+    },
+    loginSuccess: (state, { payload }) => {
+      (state.loading = false), (state.currentUser = payload?.user?.username);
+    },
   },
 });
+
+export default authSlice.reducer;
